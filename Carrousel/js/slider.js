2e079f062317f6d2466c7406fa63c784c3794e25
Cédric DE BRUYNE <cdebruyne@outlook.com>
2017-04-27 10:37:34 +0200

    27 avril

--- Carrousel/js/slider.js
+++ Carrousel/js/slider.js
@@ -4,15 +4,21 @@
 /* ****************************************** DONNEES ****************************************** */
 /*************************************************************************************************/
 var slides = [
-{ image:'', legend:''
-
+{ image:'../images/1.jpg', legend:'first image',
+ image:'../images/2.jpg', legend:'second image',
+ image:'../images/3.jpg', legend:'third image',
+ image:'../images/4.jpg', legend:'quatre image',
+ image:'../images/5.jpg', legend:'cinq image',
+ image:'../images/6.jpg', legend:'six image'
 }]
 
 
 /*************************************************************************************************/
 /* ***************************************** FONCTIONS ***************************************** */
 /*************************************************************************************************/
-
+function onClickButton () {
+  button.classList.toggle('hidden');
+}
 
 
 /*************************************************************************************************/
@@ -20,3 +26,6 @@ var slides = [
 /*************************************************************************************************/
 
 
+// Installation d'un gestionnaire d'évènement clic sur le bouton.
+var button = document.getElementByClassName('toolbar');
+button.addEventListener('click', onClickButton);
