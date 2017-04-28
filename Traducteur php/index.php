4b36b0cbcab6be818c6129244475bf83e4197c80
Marie BOSQ <bosq.marie@gmail.com>
2017-04-28 14:15:08 +0200

    second commit

--- Traducteur php/index.php
+++ Traducteur php/index.php
@@ -1,19 +1,39 @@
 <?php
 
+$dictionnaire [
+  'monde' => 'word',
+  'mer' => 'sea',
+  'arbre' => 'tree',
+  'maison' => 'house',
+  'table' => 'table',
+];
+
+$dictionnaireEn = array_flip($dictionnaire)
 var_dump ($_GET); die;
 
-$_GET['mot']
-
-function translate ()
-{
-  $dictionnaire [
-    'monde' => 'word',
-    'mer' => 'sea',
-    'arbre' => 'tree',
-    'maison' => 'house',
-    'table' => 'table',
-  ]
+$clefAtraduire = $_GET['word'];
+$direction = $_GET['direction'];
 
 
+function translate ($clefAtraduire, $direction) {
+  if ($direction == 'toEnglish')
+  switch($clefAtraduire) {
+    case 'monde';
+    echo $dictionnaire['monde'];
+    break;
+  } else {
+    switch($clefAtraduire) {
+      case "world";
+      echo $dictionnaireEn['world'];
+      break;
+    }
+  }
 }
 ?>
+
+<div>
+    <p>La traduction de :</p>
+    <p><?php echo $_POST['word'] ?></p>
+    <p>est</p>
+    <p><?php $clefAtraduire = $_GET['word']; echo $dictionnaire[$_GET[$clefAtraduire]]; ?></p>
+</div>
