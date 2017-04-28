625aaae5f9ce70ac8e5b13881c0a110e4974d7e7
Marie BOSQ <bosq.marie@gmail.com>
2017-04-28 12:55:02 +0200

    first commit

---
+++ Traducteur php/index.php
@@ -0,0 +1,19 @@
+<?php
+
+var_dump ($_GET); die;
+
+$_GET['mot']
+
+function translate ()
+{
+  $dictionnaire [
+    'monde' => 'word',
+    'mer' => 'sea',
+    'arbre' => 'tree',
+    'maison' => 'house',
+    'table' => 'table',
+  ]
+
+
+}
+?>
