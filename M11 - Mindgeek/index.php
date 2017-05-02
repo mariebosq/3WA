92b744952a7b00061ac531c06440128295e63b38
Marie BOSQ <bosq.marie@gmail.com>
2017-05-02 11:56:05 +0200

    first commit

---
+++ M11 - Mindgeek/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>TITRE DU PROJET</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<!-- CSS Vendor -->
+		<link rel="stylesheet" type="text/css" href="css/font-awesome.min.css" media="all" />
+		<link rel="stylesheet" type="text/css" href="css/normalize.css" media="all" />
+		<link href="https://fonts.googleapis.com/css?family=Candal" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">  
+		<!-- CSS Perso -->
+		<link rel="stylesheet" type="text/css" href="css/base.css" media="screen" />
+		<link rel="stylesheet" type="text/css" href="css/style.css" media="screen" />
+	</head>
+	<body>
+		<!--HEADER-->
+		<header>
+			<?php include("php/header.php"); ?>
+		</header>
+		<section id="head">
+			<h1>L'hébergement à prix libre</h1>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint</p>
+		</section>
+		<p id="search">Que recherchez-vous ?</p>
+		<!--MAIN CONTENT-->
+		<main>
+		</main>
+		<!--FOOTER-->
+		<?php include("php/footer.php"); ?>
+	</body>
+</html>
