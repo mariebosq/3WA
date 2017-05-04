6ad924b7a4f8eef0f4761863a4196941d2bc844d
Marie BOSQ <bosq.marie@gmail.com>
2017-05-04 16:50:46 +0200

    last commit

--- M12 - Cup of Tea/index.php
+++ M12 - Cup of Tea/index.php
@@ -14,6 +14,8 @@
 		<link rel="stylesheet" type="text/css" href="css/style.css" media="screen" />
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
 		<script src="js/jquery.flexslider-min.js"></script>
+
+		</script>
 	<script type="text/javascript">
 		$(window).load(function() {
 			$('.flexslider').flexslider({
@@ -41,15 +43,57 @@
 
 			<section id="thes">
 				<h2>Choisissez votre thé</h2>
-					<aside>
-						<img src="img/tea/1.jpg" alt="thé noir">
-						<img src="img/tea/2.jpg" alt="thé vert">
-						<img src="img/tea/3.jpg" alt="Oolong">
-						<img src="img/tea/4.jpg" alt="thé blanc">
-						<img src="img/tea/5.jpg" alt="Rooibos">
-					</aside>
+					<article>
+						<img src="img/tea/1.jpg" alt="thé noir" class="min">
+						<h3>Thé noir</h3>
+					</article>
+					<article>
+						<img src="img/tea/2.jpg" alt="thé vert" class="min">
+						<h3>Thé vert</h3>
+					</article>
+					<article >
+						<img src="img/tea/3.jpg" alt="Oolong" class="min">
+						<h3>Oolong</h3>
+					</article>
+					<article>
+						<img src="img/tea/4.jpg" alt="thé blanc" class="min">
+						<h3>Thé blanc</h3>
+					</article>
+					<article>
+						<img src="img/tea/5.jpg" alt="Rooibos" class="min">
+						<h3>Rooibos</h3>
+					</article>
 			</section>
 
+			<section id="presentation">
+				<article class="type">
+					<h2>Notre nouveauté</h2>
+					<img src="img/product/product1.jpg" alt="hammam">
+					<p class="legend">Thé du hammam</p>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+					<p>A partir de</p>
+					<p><strong>8,50$</strong></p>
+					<p class="bouton">voir ce produit</p>
+				</article>
+				<article class="type">
+					<h2>Notre best-seller</h2>
+					<img src="img/product/product2.jpg" alt="infusion">
+					<p class="legend">Infusion Herboriste</p>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+					<p>A partir de</p>
+					<p><strong>7,60$</strong></p>
+					<p class="bouton">voir ce produit</p>
+				</article>
+				<article class="type">
+					<h2>Notre coup de coeur</h2>
+					<img src="img/product/product3.jpg" alt="hammam">
+					<p class="legend">Blue of London</p>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+					<p>A partir de</p>
+					<p><strong>9,00$</strong></p>
+					<p class="bouton">voir ce produit</p>
+				</article>
+			</section>
 
 		</main>
 		<!--FOOTER-->
