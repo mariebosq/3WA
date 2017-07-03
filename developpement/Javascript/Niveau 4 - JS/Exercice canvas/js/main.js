ac4021bf164b95d953f56d046233dce21e48aa29
Marie BOSQ <bosq.marie@gmail.com>
2017-07-03 11:08:35 +0200

    rename canvas file

--- developpement/Javascript/Niveau 4 - JS/Exercice canvas/js/main.js
+++ developpement/Javascript/Niveau 4 - JS/Exercice canvas/js/main.js
@@ -1,5 +1,13 @@
-' use strict'
+// **********************************************************************************
+// ********************************* Code Principal *********************************
+// **********************************************************************************
 
+/*
+ * Installation d'un gestionnaire d'évènement déclenché quand l'arbre DOM sera
+ * totalement construit par le navigateur.
+ *
+ * Le gestionnaire d'évènement est une fonction anonyme que l'on donne directement à jQuery.
+ */
 $(function()
 {
     var magicalSlate;
