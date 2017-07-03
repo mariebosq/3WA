f23ad1a79a080e45d6de0507f3081b5514aea91d
Marie BOSQ <bosq.marie@gmail.com>
2017-07-03 11:06:43 +0200

    remote files

--- Projet restaurant/Projet - Code départ/application/config/database.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-/*
- * Database configuration settings used by PDO.
- */
-
-$config['dsn']      = 'mysql:host=localhost;dbname=restaurant;charset=UTF8';
-$config['password'] = 'root';
-$config['user']     = 'root';
--- Projet site e-commerce restaurant/Projet - Code Départ/application/config/database.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-/*
- * Database configuration settings used by PDO.
- */
-
-$config['dsn']      = '';
-$config['password'] = '';
-$config['user']     = '';
